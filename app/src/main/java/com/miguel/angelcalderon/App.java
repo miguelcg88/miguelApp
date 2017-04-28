427076d9bd9d1d459dd4564a96f1e49972487aec
Samuel Reque Zambrana <samo.reque@gmail.com>
2017-04-28 16:51:24 -0400

    added space

--- app/src/main/java/com/miguel/angelcalderon/App.java
+++ app/src/main/java/com/miguel/angelcalderon/App.java
@@ -24,6 +24,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         ActiveAndroid.initialize(this);
+        
 
         DefaultData defaultData = new DefaultData();
         System.out.println(defaultData.defaultDatas().size());
